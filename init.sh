92db1cf17cb064645874528a775b0925857b9758
mehariw <mehariwamlake@gmail.com>
2026-04-16 11:16:37 +0300

    last

--- init.sh
+++ init.sh
@@ -1,31 +1,39 @@
-version: "3.7"
-services:
-  mariadb:
-    image: mariadb:10.8
-    command:
-      - --character-set-server=utf8mb4
-      - --collation-server=utf8mb4_unicode_ci
-      - --skip-character-set-client-handshake
-      - --skip-innodb-read-only-compressed # Temporary fix for MariaDB 10.6
-    environment:
-      MYSQL_ROOT_PASSWORD: admindrive
-    volumes:
-      - mariadb-data:/var/lib/mysql
+#!bin/bash
 
-  redis:
-    image: redis:alpine
+if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
+    echo "Bench already exists, skipping init"
+    cd frappe-bench
+    bench start
+else
+    echo "Creating new bench..."
+fi
 
-  frappe:
-    image: frappe/bench:latest
-    command: bash /workspace/init.sh
-    environment:
-      - SHELL=/bin/bash
-    working_dir: /home/frappe
-    volumes:
-      - .:/workspace
-    ports:
-      - 8001:8000
-      - 9000:9000
+bench init --skip-redis-config-generation frappe-bench --version version-15
 
-volumes:
-  mariadb-data:
+cd frappe-bench
+
+# Use containers instead of localhost
+bench set-mariadb-host mariadb
+bench set-redis-cache-host redis:6379
+bench set-redis-queue-host redis:6379
+bench set-redis-socketio-host redis:6379
+
+# Remove redis, watch from Procfile
+sed -i '/redis/d' ./Procfile
+sed -i '/watch/d' ./Procfile
+
+bench get-app drive --branch main
+
+bench new-site drive.localhost \
+--force \
+--mariadb-root-password 123 \
+--admin-password admin \
+--no-mariadb-socket
+
+bench --site drive.localhost install-app drive
+bench --site drive.localhost set-config developer_mode 1
+bench --site drive.localhost clear-cache
+bench --site drive.localhost set-config mute_emails 1
+bench use drive.localhost
+
+bench start
